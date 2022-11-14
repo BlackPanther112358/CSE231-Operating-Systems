cdf4a9f1d3f288f8335b7de9708310a30a0d14dd
BlackPanther112358 <jainaakarsh09@gmail.com>
2022-11-15 03:13:03 +0530

    Code after day 1

--- Ass_2/Q1/count.c
+++ Ass_2/Q1/count.c
@@ -8,12 +8,14 @@
 #include <sys/wait.h>
 #include <unistd.h>
 #include <pthread.h>
-#include <sys/time.h>
+#include <time.h>
 
 pthread_t tid1, tid2, tid3;
 struct sched_param p1;
 struct sched_param p2;
 struct sched_param p3;
+struct timespec begin[3];
+struct timespec end[3];
 
 void* countA(void* arg){
     p1.sched_priority = 0;
@@ -41,13 +43,26 @@ void* countC(void* arg){
 
 int main(){
 
+    clock_gettime(CLOCK_REALTIME, &begin[0]);
     pthread_create(&tid1, NULL, countA, NULL);
+    clock_gettime(CLOCK_REALTIME, &begin[1]);
     pthread_create(&tid2, NULL, countB, NULL);
+    clock_gettime(CLOCK_REALTIME, &begin[2]);
     pthread_create(&tid3, NULL, countC, NULL);
 
     pthread_join(tid1, NULL);
+    clock_gettime(CLOCK_REALTIME, &end[0]);
     pthread_join(tid2, NULL);
+    clock_gettime(CLOCK_REALTIME, &end[1]);
     pthread_join(tid3, NULL);
+    clock_gettime(CLOCK_REALTIME, &end[2]);
+
+    printf("%lds %ldms\n", begin[0].tv_sec, begin[0].tv_nsec);
+    printf("%lds %ldms\n", begin[1].tv_sec, begin[1].tv_nsec);
+    printf("%lds %ldms\n", begin[2].tv_sec, begin[2].tv_nsec);
+    printf("%lds %ldms\n", end[0].tv_sec, end[0].tv_nsec);
+    printf("%lds %ldms\n", end[1].tv_sec, end[1].tv_nsec);
+    printf("%lds %ldms\n", end[2].tv_sec, end[2].tv_nsec);
 
     return 0;
 
