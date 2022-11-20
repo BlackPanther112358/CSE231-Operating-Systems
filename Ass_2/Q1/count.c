cc8f4c3fead4684495a7f665de4251ff15bf393e
BlackPanther112358 <jainaakarsh09@gmail.com>
2022-11-20 05:51:39 +0530

    Added code to calculate time-delta

--- Ass_2/Q1/count.c
+++ Ass_2/Q1/count.c
@@ -4,58 +4,62 @@
 #include <math.h>
 #include <limits.h>
 #include <stdlib.h>
-#include <sys/types.h>
-#include <sys/wait.h>
-#include <unistd.h>
 #include <pthread.h>
 #include <time.h>
 
 pthread_t tid1, tid2, tid3;
+int OTHER_PRIORITY, RR_PRIORITY, FIFO_PRIORITY;
 struct sched_param p1;
 struct sched_param p2;
 struct sched_param p3;
 struct timespec begin[3];
 struct timespec end[3];
+float time_val[3];
 
 void* countA(void* arg){
-    p1.sched_priority = 0;
+    clock_gettime(CLOCK_REALTIME, &begin[0]);
+    p1.sched_priority = OTHER_PRIORITY;
     pthread_setschedparam(tid1, (int)SCHED_OTHER, &p1);
     long long int up_limit = (long long int)1<<32, cur_cnt = 0;
     for(long long int i = 0; i < up_limit; i++) cur_cnt++;
-    printf("A finished\n");
+    clock_gettime(CLOCK_REALTIME, &end[0]);
 }
 
 void* countB(void* arg){
-    p2.sched_priority = 0;
+    clock_gettime(CLOCK_REALTIME, &begin[1]);
+    p2.sched_priority = RR_PRIORITY;
     pthread_setschedparam(tid2, (int)SCHED_RR, &p2);
     long long int up_limit = (long long int)1<<32, cur_cnt = 0;
     for(long long int i = 0; i < up_limit; i++) cur_cnt++;
-    printf("B finished\n");
+    clock_gettime(CLOCK_REALTIME, &end[1]);
 }
 
 void* countC(void* arg){
-    p3.sched_priority = 0;
+    clock_gettime(CLOCK_REALTIME, &begin[2]);
+    p3.sched_priority = FIFO_PRIORITY;
     pthread_setschedparam(tid3, (int)SCHED_FIFO, &p3);
     long long int up_limit = (long long int)1<<32, cur_cnt = 0;
     for(long long int i = 0; i < up_limit; i++) cur_cnt++;
-    printf("C finished\n");
+    clock_gettime(CLOCK_REALTIME, &end[2]);
 }
 
-int main(){
+int main(int argc, char* argv[]){
 
-    clock_gettime(CLOCK_REALTIME, &begin[0]);
+    OTHER_PRIORITY = atoi(argv[1]);
+    RR_PRIORITY = atoi(argv[2]);
+    FIFO_PRIORITY = atoi(argv[3]);
+
     pthread_create(&tid1, NULL, countA, NULL);
-    clock_gettime(CLOCK_REALTIME, &begin[1]);
     pthread_create(&tid2, NULL, countB, NULL);
-    clock_gettime(CLOCK_REALTIME, &begin[2]);
     pthread_create(&tid3, NULL, countC, NULL);
 
     pthread_join(tid1, NULL);
-    clock_gettime(CLOCK_REALTIME, &end[0]);
     pthread_join(tid2, NULL);
-    clock_gettime(CLOCK_REALTIME, &end[1]);
     pthread_join(tid3, NULL);
-    clock_gettime(CLOCK_REALTIME, &end[2]);
+
+    time_val[0] = (end[0].tv_sec - begin[0].tv_sec) + ((float)end[0].tv_nsec - begin[0].tv_nsec)/1000000000;
+    time_val[1] = (end[1].tv_sec - begin[1].tv_sec) + ((float)end[1].tv_nsec - begin[1].tv_nsec)/1000000000;
+    time_val[2] = (end[2].tv_sec - begin[2].tv_sec) + ((float)end[2].tv_nsec - begin[2].tv_nsec)/1000000000;
 
     printf("%lds %ldms\n", begin[0].tv_sec, begin[0].tv_nsec);
     printf("%lds %ldms\n", begin[1].tv_sec, begin[1].tv_nsec);
@@ -63,6 +67,9 @@ int main(){
     printf("%lds %ldms\n", end[0].tv_sec, end[0].tv_nsec);
     printf("%lds %ldms\n", end[1].tv_sec, end[1].tv_nsec);
     printf("%lds %ldms\n", end[2].tv_sec, end[2].tv_nsec);
+    printf("%f s\n", time_val[0]);
+    printf("%f s\n", time_val[1]);
+    printf("%f s\n", time_val[2]);
 
     return 0;
 
