1c627d96913e2c09225290f7029e40b08d0401b9
BlackPanther112358 <jainaakarsh09@gmail.com>
2022-11-21 17:13:47 +0530

    Added more code

--- Ass_2/Q1/compile.c
+++ Ass_2/Q1/compile.c
@@ -2,6 +2,7 @@
 #include <stdbool.h>
 #include <string.h>
 #include <limits.h>
+#include <math.h>
 #include <stdlib.h>
 #include <sys/types.h>
 #include <sys/wait.h>
@@ -13,19 +14,21 @@ struct timespec begin[3];
 struct timespec end[3];
 float time_val[3];
 
-const char* SCRIPT_A = "bash runA.sh";
-const char* SCRIPT_B = "bash runB.sh";
-const char* SCRIPT_C = "bash runC.sh";
+const char* SCRIPT_A = "./runA.sh";
+const char* SCRIPT_B = "./runB.sh";
+const char* SCRIPT_C = "./runC.sh";
 
 void setPid(int idx, pid_t pid){
     pids[idx] = pid;
 }
 
-void setBegin(int idx, timespec start_time){
+void setBegin(int idx, struct timespec start_time){
+    printf("Setting %d at %lds %ld ns\n", idx, start_time.tv_sec, start_time.tv_nsec);
     begin[idx] = start_time;
 }
 
-void setEnd(pid_t pid, timespec end_time){
+void setEnd(pid_t pid, struct timespec end_time){
+    printf("Setting %d at %lds %ld ns\n", pid, end_time.tv_sec, end_time.tv_nsec);
     for(int i = 0; i < 3; i++) if(pids[i] == pid) end[i] = end_time;
 }
 
@@ -36,22 +39,29 @@ int main(){
 
     for(int i = 0; i < 3; i++){
         if((child_pid = fork()) == 0){
-            if(i == 0) {execl(SCRIPT_A, (char *)NULL); printf("Called A\n");}
-            else if(i == 1) execl(SCRIPT_B, (char *)NULL);
-            else if(i == 2) execl(SCRIPT_C, (char *)NULL);
+            if(i == 0) {struct timespec curr_time; clock_gettime(CLOCK_REALTIME, &curr_time); setBegin(i, curr_time); execvp(SCRIPT_A, NULL);}
+            else if(i == 1) {struct timespec curr_time; clock_gettime(CLOCK_REALTIME, &curr_time); setBegin(i, curr_time); execvp(SCRIPT_B, NULL);}
+            else if(i == 2) {struct timespec curr_time; clock_gettime(CLOCK_REALTIME, &curr_time); setBegin(i, curr_time); execvp(SCRIPT_C, NULL);}
             exit(0);
-        }else printf("%d\n", child_pid);
+        }else {setPid(i, child_pid); printf("%d\n", child_pid);}
     }
 
-    while((wpid = wait(&status)) > 0) printf("%d\n", wpid);
+    // Set endtimes here
+    while((wpid = wait(&status)) > 0) {struct timespec curr_time; clock_gettime(CLOCK_REALTIME, &curr_time); setEnd(wpid, curr_time); printf("ENDING: %lds %ldms\n", curr_time.tv_sec, curr_time.tv_nsec);}
 
     time_val[0] = (end[0].tv_sec - begin[0].tv_sec) + ((float)end[0].tv_nsec - begin[0].tv_nsec)/1000000000;
     time_val[1] = (end[1].tv_sec - begin[1].tv_sec) + ((float)end[1].tv_nsec - begin[1].tv_nsec)/1000000000;
     time_val[2] = (end[2].tv_sec - begin[2].tv_sec) + ((float)end[2].tv_nsec - begin[2].tv_nsec)/1000000000;
 
-    printf("%f s\n", time_val[0]);
-    printf("%f s\n", time_val[1]);
-    printf("%f s\n", time_val[2]);
+    printf("%lds %ldms\n", begin[0].tv_sec, begin[0].tv_nsec);
+    printf("%lds %ldms\n", begin[1].tv_sec, begin[1].tv_nsec);
+    printf("%lds %ldms\n", begin[2].tv_sec, begin[2].tv_nsec);
+    printf("%lds %ldms\n", end[0].tv_sec, end[0].tv_nsec);
+    printf("%lds %ldms\n", end[1].tv_sec, end[1].tv_nsec);
+    printf("%lds %ldms\n", end[2].tv_sec, end[2].tv_nsec);
+    printf("%ld s\n", time_val[0]);
+    printf("%ld s\n", time_val[1]);
+    printf("%ld s\n", time_val[2]);
 
     return 0;
 
