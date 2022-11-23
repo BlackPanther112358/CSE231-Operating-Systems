d8ce3fb1e434b5c9533aa7c2367853869c581493
BlackPanther112358 <jainaakarsh09@gmail.com>
2022-11-23 22:14:16 +0530

    q2 template now working

--- Ass_2/Q1/compile.c
+++ Ass_2/Q1/compile.c
@@ -23,12 +23,10 @@ void setPid(int idx, pid_t pid){
 }
 
 void setBegin(int idx, struct timespec start_time){
-    printf("Setting %d at %lds %ld ns\n", idx, start_time.tv_sec, start_time.tv_nsec);
     begin[idx] = start_time;
 }
 
 void setEnd(pid_t pid, struct timespec end_time){
-    printf("Setting %d at %lds %ld ns\n", pid, end_time.tv_sec, end_time.tv_nsec);
     for(int i = 0; i < 3; i++) if(pids[i] == pid) end[i] = end_time;
 }
 
@@ -38,16 +36,18 @@ int main(){
     int status = 0;
 
     for(int i = 0; i < 3; i++){
+        struct timespec curr_time; 
+        clock_gettime(CLOCK_REALTIME, &curr_time); 
+        setBegin(i, curr_time);
         if((child_pid = fork()) == 0){
-            if(i == 0) {struct timespec curr_time; clock_gettime(CLOCK_REALTIME, &curr_time); setBegin(i, curr_time); execvp(SCRIPT_A, NULL);}
-            else if(i == 1) {struct timespec curr_time; clock_gettime(CLOCK_REALTIME, &curr_time); setBegin(i, curr_time); execvp(SCRIPT_B, NULL);}
-            else if(i == 2) {struct timespec curr_time; clock_gettime(CLOCK_REALTIME, &curr_time); setBegin(i, curr_time); execvp(SCRIPT_C, NULL);}
+            if(i == 0) execvp(SCRIPT_A, NULL);
+            else if(i == 1) execvp(SCRIPT_B, NULL);
+            else if(i == 2) execvp(SCRIPT_C, NULL);
             exit(0);
-        }else {setPid(i, child_pid); printf("%d\n", child_pid);}
+        }else setPid(i, child_pid);
     }
 
-    // Set endtimes here
-    while((wpid = wait(&status)) > 0) {struct timespec curr_time; clock_gettime(CLOCK_REALTIME, &curr_time); setEnd(wpid, curr_time); printf("ENDING: %lds %ldms\n", curr_time.tv_sec, curr_time.tv_nsec);}
+    while((wpid = wait(&status)) > 0) {struct timespec curr_time; clock_gettime(CLOCK_REALTIME, &curr_time); setEnd(wpid, curr_time);}
 
     time_val[0] = (end[0].tv_sec - begin[0].tv_sec) + ((float)end[0].tv_nsec - begin[0].tv_nsec)/1000000000;
     time_val[1] = (end[1].tv_sec - begin[1].tv_sec) + ((float)end[1].tv_nsec - begin[1].tv_nsec)/1000000000;
@@ -59,9 +59,9 @@ int main(){
     printf("%lds %ldms\n", end[0].tv_sec, end[0].tv_nsec);
     printf("%lds %ldms\n", end[1].tv_sec, end[1].tv_nsec);
     printf("%lds %ldms\n", end[2].tv_sec, end[2].tv_nsec);
-    printf("%ld s\n", time_val[0]);
-    printf("%ld s\n", time_val[1]);
-    printf("%ld s\n", time_val[2]);
+    printf("%f s\n", time_val[0]);
+    printf("%f s\n", time_val[1]);
+    printf("%f s\n", time_val[2]);
 
     return 0;
 
