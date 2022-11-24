75b3d7765f5ef742e54bc7d7e04e31124c6e7aec
BlackPanther112358 <jainaakarsh09@gmail.com>
2022-11-25 04:48:03 +0530

    wrotealmost all e code for shm

--- Ass_3/Q2/shm_p1.c
+++ Ass_3/Q2/shm_p1.c
@@ -1,9 +1,11 @@
+#include <stdio.h>
 #include <stdbool.h>
 #include <string.h>
 #include <math.h>
 #include <limits.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <semaphore.h>
 #include <fcntl.h>
 #include <errno.h>
 #include <sys/stat.h>
@@ -15,7 +17,12 @@
 #define array_size 50
 #define string_length 10
 #define block_size 5
+#define SHM_SIZE 1024
+#define SHM_NAME "/tmp"
+#define SEM_NAME "sem_shm2"
 
+key_t SHM_KEY;
+sem_t *sem;
 char string_array[array_size][string_length + 1];
 
 void generate_string_array(){
@@ -40,7 +47,78 @@ int main(){
     generate_string_array();
     // print_string_array();
 
+    SHM_KEY = ftok(SHM_NAME, 'A');
+    if(SHM_KEY == -1) {perror("ftok error"); exit(1);}
+    int shmid = shmget(SHM_KEY, SHM_SIZE, IPC_CREAT | 0666);    
+    if(shmid == -1) {perror("shmget error"); exit(1);}
 
+    char *shm = shmat(shmid, NULL, 0);
+    if(shm == (char *)-1) {perror("shmat error"); exit(1);}
+
+    sem = sem_open(SEM_NAME, O_CREAT, 0666, 1);
+    if(sem == SEM_FAILED) {perror("sem_open error"); exit(1);}
+
+    printf("HERE\n");
+
+    sem_wait(sem);
+
+    printf("HERE\n");
+
+    char data[5*(string_length + 4)] = {0};
+    for(int i = 0; i < block_size; i++){
+        char num[1];
+        sprintf(num, "%d", i + 1);
+        strcat(data, "0");
+        strcat(data, num);
+        strcat(data, " ");
+        strcat(data, string_array[i]);
+    }
+    sprintf(shm, "%s", data);
+    printf("Sent: %s\n", data);
+
+    sem_post(sem);
+    sleep(10);
+
+    int index = 0;
+
+    while(index < array_size){
+        sem_wait(sem);
+
+        index = atoi(shm);
+        printf("Recieved: %d\n", index);
+
+        if(index >= array_size){
+            printf("All strings sent\n");
+            sem_post(sem);
+            break;
+        }
+
+        char data[5*(string_length + 4)] = {0};
+        for(int i = 0; i < block_size; i++){
+            if((index + i)< 9){
+                char num[1];
+                sprintf(num, "%d", index + i + 1);
+                strcat(data, "0");
+                strcat(data, num);
+                strcat(data, " ");
+                strcat(data, string_array[index + i]);
+            }else{
+                char num[2];
+                sprintf(num, "%d", index + i + 1);
+                strcat(data, num);
+                strcat(data, " ");
+                strcat(data, string_array[index + i]);
+            }
+        }
+        sprintf(shm, "%s", data);
+        printf("Sent: %s\n", data);
+
+        sem_post(sem);
+
+        sleep(5);
+    }
+
+    sem_unlink(SEM_NAME);
 
     return 0;
 
--- Ass_3/Q2/shm_p2.c
+++ Ass_3/Q2/shm_p2.c
@@ -1,9 +1,11 @@
+#include <stdio.h>
 #include <stdbool.h>
 #include <string.h>
 #include <math.h>
 #include <limits.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <semaphore.h>
 #include <fcntl.h>
 #include <errno.h>
 #include <sys/stat.h>
@@ -15,12 +17,60 @@
 #define array_size 50
 #define string_length 10
 #define block_size 5
+#define SHM_SIZE 1024
+#define SHM_NAME "/tmp"
+#define SEM_NAME "sem_shm2"
 
+key_t SHM_KEY;
+sem_t *sem;
 char string_array[array_size][string_length + 1];
 
 int main(){
 
-    
+    sleep(5);
+
+    SHM_KEY = ftok(SHM_NAME, 'A');
+    if(SHM_KEY == -1) {perror("ftok error"); exit(1);}
+    int shmid = shmget(SHM_KEY, SHM_SIZE, 0666);    
+    if(shmid == -1) {perror("shmget error"); exit(1);}
+
+    char *shm = shmat(shmid, NULL, 0);
+    if(shm == (char *)-1) {perror("shmat error"); exit(1);}
+
+    sem = sem_open(SEM_NAME, 0);
+    if(sem == SEM_FAILED) {perror("sem_open error"); exit(1);}
+
+    int index = 0;
+
+    while(index < array_size){
+        sem_wait(sem);
+
+        char data[5*(string_length + 1)] = {0};
+        memcpy(data, shm, 5*(string_length + 1));
+        printf("Recieved %s\n", data);
+        int itr = 0;
+        for(int i = 0; i < block_size; i++){
+            char idx[2];
+            idx[0] = data[itr++];
+            idx[1] = data[itr++];
+            idx[2] = '\0';
+            itr++;
+            index = atoi(idx);
+            for(int j = 0; j < string_length; j++){
+                string_array[index - 1][j] = data[itr];
+                itr++;
+            }
+        }
+
+        sprintf(shm, "%d", index);
+        printf("Sent %d\n", index);
+
+        sem_post(sem);
+
+        sleep(5);
+    }
+
+    sem_unlink(SEM_NAME);
 
     return 0;
 
