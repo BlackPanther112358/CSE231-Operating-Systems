6093045065143007a1216bb6ac3ef3e9f908cfe8
BlackPanther112358 <jainaakarsh09@gmail.com>
2022-12-18 03:07:09 +0530

    Added code for q1 with semaphores

---
+++ Ass_3/Q1/sem_a.c
@@ -0,0 +1,97 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <string.h>
+#include <math.h>
+#include <limits.h>
+#include <stdlib.h>
+#include <time.h>
+#include <pthread.h>
+#include <unistd.h>
+#include <semaphore.h>
+
+#define PHILOSOPHER_CNT 5   // Set number of Philosophers
+#define HUNGER 2            // Set number of times each philosopher eats
+
+struct FORK{
+    int id;
+    sem_t *fork;
+}typedef FORK;
+
+struct PHILOSOPHER{
+    int id;
+    pthread_t pid;
+    int parity;
+    int hunger;
+    sem_t *left_fork;
+    sem_t *right_fork;
+}typedef PHILOSOPHER;
+
+FORK forks[PHILOSOPHER_CNT];
+PHILOSOPHER philosophers[PHILOSOPHER_CNT];
+
+int get_random_integer(){
+    return 10 + rand() % 10;
+}
+
+void generate_forks(){
+    for(int i = 0; i < PHILOSOPHER_CNT; i++){
+        forks[i].id = i;
+        forks[i].fork = (sem_t *)malloc(sizeof(sem_t));
+        sem_init(forks[i].fork, 0, 1);
+    }
+}
+
+void generate_philosophers(){
+    for(int i = 0; i < PHILOSOPHER_CNT; i++){
+        philosophers[i].id = i;
+        philosophers[i].parity = (i == 4) ? 1 : 0;
+        philosophers[i].pid = (pthread_t)i;
+        philosophers[i].hunger = HUNGER;
+        philosophers[i].left_fork = forks[i].fork;
+        philosophers[i].right_fork = forks[(i + 1) % PHILOSOPHER_CNT].fork;
+    }
+}
+
+void* philosopher(void *arg){
+    PHILOSOPHER *philosopher = (PHILOSOPHER *)arg;
+    while(philosopher->hunger--){
+        if(philosopher->parity == 0){
+            sem_wait(philosopher->left_fork);
+            sem_wait(philosopher->right_fork);
+        }else{
+            sem_wait(philosopher->right_fork);
+            sem_wait(philosopher->left_fork);
+        }
+        printf("Philosopher %d is eating\n", philosopher->id);
+        sleep(get_random_integer());
+        sem_post(philosopher->left_fork);
+        sem_post(philosopher->right_fork);
+        printf("Philosopher %d is thinking\n", philosopher->id);
+        sleep(get_random_integer());
+    }
+    printf("Philosopher %d leaves the table\n", philosopher->id);
+    return NULL;
+}
+
+int main(){
+
+    srand(time(NULL));
+
+    generate_forks();
+    generate_philosophers();
+
+    pthread_create(&philosophers[0].pid, NULL, philosopher, &philosophers[0]);
+    pthread_create(&philosophers[1].pid, NULL, philosopher, &philosophers[1]);
+    pthread_create(&philosophers[2].pid, NULL, philosopher, &philosophers[2]);
+    pthread_create(&philosophers[3].pid, NULL, philosopher, &philosophers[3]);
+    pthread_create(&philosophers[4].pid, NULL, philosopher, &philosophers[4]);
+
+    pthread_join(philosophers[0].pid, NULL);
+    pthread_join(philosophers[1].pid, NULL);
+    pthread_join(philosophers[2].pid, NULL);
+    pthread_join(philosophers[3].pid, NULL);
+    pthread_join(philosophers[4].pid, NULL);
+
+    return 0;
+
+}
