9afc3a6e70ad6a03d26a536c28fd62233f2b867e
BlackPanther112358 <jainaakarsh09@gmail.com>
2022-12-18 04:34:42 +0530

    Added sauce bowl for semaphore code

--- Ass_3/Q1/sem_b.c
+++ Ass_3/Q1/sem_b.c
@@ -11,11 +11,17 @@
 
 #define PHILOSOPHER_CNT 5   // Set number of Philosophers
 #define HUNGER 2            // Set number of times each philosopher eats
+#define BOWL_CNT 2          // Set number of bowls
 
 typedef struct FORK{
     int id;
     sem_t *fork;
 }FORK;
+
+typedef struct BOWL{
+    int id;
+    sem_t *bowl;
+}BOWL;
 
 typedef struct PHILOSOPHER{
     int id;
@@ -24,9 +30,11 @@ typedef struct PHILOSOPHER{
     int hunger;
     sem_t *left_fork;
     sem_t *right_fork;
+    sem_t *bowl;
 }PHILOSOPHER;
 
 FORK forks[PHILOSOPHER_CNT];
+BOWL bowls[BOWL_CNT];
 PHILOSOPHER philosophers[PHILOSOPHER_CNT];
 
 int get_random_integer(){
@@ -41,6 +49,14 @@ void generate_forks(){
     }
 }
 
+void generate_bowls(){
+    for(int i = 0; i < BOWL_CNT; i++){
+        bowls[i].id = i;
+        bowls[i].bowl = (sem_t *)malloc(sizeof(sem_t));
+        sem_init(bowls[i].bowl, 0, 1);
+    }
+}
+
 void generate_philosophers(){
     for(int i = 0; i < PHILOSOPHER_CNT; i++){
         philosophers[i].id = i;
@@ -49,6 +65,7 @@ void generate_philosophers(){
         philosophers[i].hunger = HUNGER;
         philosophers[i].left_fork = forks[i].fork;
         philosophers[i].right_fork = forks[(i + 1) % PHILOSOPHER_CNT].fork;
+        philosophers[i].bowl = NULL;
     }
 }
 
@@ -62,10 +79,15 @@ void* philosopher_simulate(void *arg){
             sem_wait(philosopher->right_fork);
             sem_wait(philosopher->left_fork);
         }
+        int val = -1;
+        sem_getvalue(bowls[0].bowl, &val);
+        if(val) {sem_wait(bowls[0].bowl); philosopher->bowl = bowls[0].bowl;}
+        else {sem_wait(bowls[1].bowl); philosopher->bowl = bowls[1].bowl;}
         printf("Philosopher %d is eating\n", philosopher->id);
         sleep(get_random_integer());
         sem_post(philosopher->left_fork);
         sem_post(philosopher->right_fork);
+        sem_post(philosopher->bowl);
         printf("Philosopher %d is thinking\n", philosopher->id);
         sleep(get_random_integer());
     }
@@ -78,6 +100,7 @@ int main(){
     srand(time(NULL));
 
     generate_forks();
+    generate_bowls();
     generate_philosophers();
 
     pthread_create(&philosophers[0].pid, NULL, philosopher_simulate, &philosophers[0]);
